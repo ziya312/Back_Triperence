1d2e6304708dab74d52b501696812aa6634e8853
ziya312 <manjak312@gmail.com>
2022-07-31 23:20:22 +0900

    .

--- TriperenceBack/src/main/java/com/example/triperenceback/dto/Survey.java
+++ TriperenceBack/src/main/java/com/example/triperenceback/dto/Survey.java
@@ -3,25 +3,29 @@ package com.example.triperenceback.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.sql.Timestamp;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class Survey {
-    private Long user_id;
-    private String user_email;
+//    private Long user_id;
+//    private String user_email;
     private Date testtime;
     private String transportation;
     private String stay;
     private String destination;
-    private String allergie;
-    private String eat;
+//    private ArrayList<String> allergie;
+//    private ArrayList<String> eat;
     private String category;
     /** k-culture **/
-    private String k_pop;
+//    private ArrayList<String> k_pop;
     private String k_movie_drama;
     /** photo **/
     private String shoot;
@@ -37,4 +41,6 @@ public class Survey {
     /** food **/
     private String foodOne;
     private String foodTwo;
+
+
 }
